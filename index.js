8bc041b8c8bba3795774e11e6577d05e553bbe94
Mitchell Walker <eagleye1144@gmail.com>
2021-01-27 14:21:09 -0700

    Task 1 complete while working on task 2

--- index.js
+++ index.js
@@ -39,13 +39,27 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  Person.prototype.eat = function (edible) {
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
+  Person.prototype.poop = function() {
+    this.stomach = [];
   }
  
- 
+ Person.prototype.toString = function () {
+  return (`${this.name}, ${this.age}`);
+ }
 
-  
+  const newPerson = new Person('Mitch', 28);
+
+  console.log(newPerson.toString());
   
   
   
@@ -63,8 +77,15 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odemeter = 0;
+  }
+
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
   }
   
   
